aba19c06fcd02fad797aa377f4914398bb534804
fakersl <zgustovo13365@gmail.com>
2024-03-03 22:01:40 -0300

    Update cadastro.php
    Co-Authored-By: Jôffil <65688137+joflakesoj@users.noreply.github.com>

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -3,14 +3,14 @@ include '../includes/includes.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
-    $email = $_POST["cpf"];
+    $cpf = $_POST["cpf"];
     $email = $_POST["email"];
     $endereco = $_POST["endereco"];
     $cidade = $_POST["cidade"];
     $estado = $_POST["estado"];
     $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+    $sql = "INSERT INTO formulario (nome, cpf, email, endereco, cidade, senha) VALUES ('$nome', '$cpf', '$email', '$endereco', '$cidade', '$senha)";
 
     if ($conexao->query($sql) === TRUE) {
         header('location: ../login/login.html');
