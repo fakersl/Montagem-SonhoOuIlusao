84d9cd6c4fc3a7408dddc866d4617415d7c84857
fakersl <zgustovo13365@gmail.com>
2024-03-03 22:06:26 -0300

    Update includes.php
    Co-Authored-By: Jôffil <65688137+joflakesoj@users.noreply.github.com>

--- includes/includes.php
+++ includes/includes.php
@@ -3,11 +3,14 @@
 $servername = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'formulario'
+$dbname = 'formulario';
 
 $conexao = new mysqli($servername,$username,$password,$dbname);
 
-if ($conexao->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
+if ($conexao->connect_errno) {
+    echo "Sucesso na conexão com o banco de dados";
+} else {
+    die("Erro na conexão com o banco de dados: " . $conexao->connect_errno);
 }
+
 ?>
