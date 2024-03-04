b5c52dc5331a292bc37427758275b68e50969872
fakersl <zgustovo13365@gmail.com>
2024-03-04 20:14:40 -0300

    Organizacao de Arquivos

--- login/login.php
+++ login/login.php
@@ -1,22 +1,23 @@
 <?php
+session_start();
 include '../includes/includes.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    $sql = "SELECT id, nome, senha FROM usuario WHERE email = '$email";
+    $sql = "SELECT id, nome, senha FROM usuario WHERE email = '$email' AND senha = '$senha'";
     $result = $conexao->query($sql);
 
     if ($result->num_rows > 0){ 
         $row = $result->fetch_assoc();
         if (password_verify($senha, $row["senha"])) {
             header('location: ../home/home.html');
-        } else {
+        } else {    
             echo "Senha incorreta!";
         }
     } else {
-        echo "Usuário não encontrado!"ç
+        echo "Usuário não encontrado!";
     }
 }
 
